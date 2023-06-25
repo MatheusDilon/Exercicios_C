aeed8ca462a6e3409c6af2c2b694555e1d0943cb
Matheus Dilon <mdilon2811@gmail.com>
2023-06-25 16:23:56 -0300

    Exercicios C

--- exercicio8_array.c
+++ exercicio8_array.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
 
-<<<<<<< HEAD
 int main(void){
-    int vetor[6] = {0}, inverso[6] = {0}, tamanho;
-=======
-int main(){
     int vetor[6] = {0}, reverso[6] = {0}, tamanho;
->>>>>>> 33c726e65ef75ea5e1b71fc1c0e14c184b7f224e
     printf("Digite 6 valores inteiros:\n");
     tamanho = sizeof(vetor)/sizeof(vetor[0]);
     for(int i = 0; i < tamanho; i++){
